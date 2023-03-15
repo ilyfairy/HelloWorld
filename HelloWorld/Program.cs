1d8a32c3e1099496fdc22181f02e1aeca81ff86c
小仙女 <me@ilyfairy.com>
2023-03-15 21:24:00 +0800

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,11 +2,14 @@
 using Auto = System.Object;
 namespace HelloWorld;
 [SkipLocalsInit]
-internal class Program
+internal class Program(string val = " ")
 {
-    public static string Val = $"{" "[0..0]}";
+    public string Val = $"{val[0..0]}";
+    private static Lazy<Program> _val = new(() => new());
+    public static Program Instance => _val.Value;
     [ModuleInitializer]
-    public static async void A1()
+    public static void AA2() => Instance.A1();
+    public async void A1()
     {
         Random? rand = null;
         int s = 0;
@@ -14,9 +17,10 @@ internal class Program
         Val += $"{Val != Val[..]}"[2];
         AppDomain.CurrentDomain.ProcessExit += async (_, _) => { foreach (char item in ^36..0x10008) await Console.Out.WriteAsync(item); };
     }
-    static async Task Main(string[] args)
+    static void Main(string[] args) => Instance.Main2();
+    public Action Main2 => async () =>
     {
-        static unsafe string? F()
+        unsafe string? F()
         {
             fixed (char* p = "") p[0] = (char)(int.Parse(new string(Enumerable.Repeat(Enumerable.Range(1, 1).First(), 3).Select(v => $"{v}".First()).ToArray())) * (*((int*)p - 1) = 1));
             return Val;
@@ -25,24 +29,24 @@ internal class Program
         await foreach (var item in A()) Console.Write(item);
         Auto v2 = 0;
         for (int i = 0; i < 27; i++) v2 = B();
-        Console.Write(await (int)v2);
+        Console.Write(await(int)v2);
         ((Action)(() =>
         {
             unsafe
             {
                 var r = __makeref(Val);
-                var addr = 1 + (nint*)*(nint*)*(nint*)&r;
+                var addr = 1 + **(nint***)&r;
                 for (int i = 37; i <= 38; i++) Console.Write((char)(*(int*)addr * i));
             }
         }))();
-    }
-    static async IAsyncEnumerable<Auto> A()
+    };
+    async IAsyncEnumerable<Auto> A()
     {
         yield return await (1L.GetType().Name.First() + 0x23);
         yield return ((Func<char>)(() => string.Empty.FirstOrDefault()))();
         yield return await await (1, 5);
     }
-    static unsafe int B()
+    unsafe int B()
     {
         int* val = stackalloc int[2];
         if (val[0] != 12345678)
