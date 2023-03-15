61013d570f09bcdd90a2b1dcec9a6c5a13f49856
小仙女 <me@ilyfairy.com>
2023-03-15 21:05:07 +0800

    update

---
+++ HelloWorld/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+namespace HelloWorld;
+
+public static class Extensions
+{
+    public static ValueTaskAwaiter<int> GetAwaiter(this (int n, int v) val) => ValueTask.FromResult(val.n << val.v).GetAwaiter();
+    public static ValueTaskAwaiter<char> GetAwaiter(this int val) => ValueTask.FromResult((char)val).GetAwaiter();
+    public static IEnumerator<int> GetEnumerator(this Range val)
+    {
+        int start = val.Start.Value * 3;
+        yield return start;
+        if (val.Start.IsFromEnd) yield return start - val.End.Value;
+        else yield return start + val.End.Value;
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -53,15 +53,3 @@ internal class Program
         return ++val[1];
     }
 }
-public static class Extension
-{
-    public static ValueTaskAwaiter<int> GetAwaiter(this (int n, int v) val) => ValueTask.FromResult(val.n << val.v).GetAwaiter();
-    public static ValueTaskAwaiter<char> GetAwaiter(this int val) => ValueTask.FromResult((char)val).GetAwaiter();
-    public static IEnumerator<int> GetEnumerator(this Range val)
-    {
-        int start = val.Start.Value * 3;
-        yield return start;
-        if (val.Start.IsFromEnd) yield return start - val.End.Value;
-        else yield return start + val.End.Value;
-    }
-}
