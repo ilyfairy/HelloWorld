2e1735ec9fd790b2ea99e6adec12783813a4bf16
小仙女 <me@ilyfairy.com>
2023-04-22 11:18:16 +0800

    update

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,32 +1,36 @@
 ﻿using System.Runtime.CompilerServices;
-using let = dynamic;
+using auto = dynamic;
 namespace HelloWorld;
 [SkipLocalsInit]
-internal class Program(string val = " ")
+internal class Program(string value = " ")
 {
-    public string Val = $"{val[0..0]}  ";
-    private readonly static Lazy<Program> _val = new(() => new());
-    public static Program Instance => _val.Value;
+    public string Value = $"{value[0..0]}  ";
+    private readonly static Lazy<Program> _instance = new(() => new());
+    public static Program Instance => _instance.Value;
     [ModuleInitializer]
     public static void AA2() => Instance.A1();
     public async void A1()
     {
         (Random? rand, int s) = (default, default);
-        for (int i = 0; i < 2; i++) Val += await ((s = (rand = rand is null ? new(887464974) : new(s)).Next(0, int.MaxValue)) % 63 + 65);
+        for (int i = 0; i < 2; i++) Value += await ((s = (rand = rand is null ? new(887464974) : new(s)).Next(0, int.MaxValue)) % 63 + 65);
         unsafe { *((nint*)typeof(uint?).TypeHandle.Value + 11) = typeof(int?).TypeHandle.Value; }
-        Val += $"{Val != Val[..]}"[$"{typeof(uint?)}".Count(v => v == '`')];
-        AppDomain.CurrentDomain.ProcessExit += async (_, _) => { foreach (char item in ^36..0x10008) await Console.Out.WriteAsync(item); };
+        Value += $"{Value != Value[..]}"[$"{typeof(uint?)}".Count(v => v == '`')];
+        AppDomain.CurrentDomain.ProcessExit += async (_, _) => 
+        {
+            foreach (char item in ^36..0x10008) await Console.Out.WriteAsync(item);
+        };
     }
     static void Main(string[] args) => Instance.Main2();
     public Action Main2 => async () =>
     {
         unsafe string? F()
         {
-            **(nint**)Unsafe.AsPointer(ref Val) = **(nint**)Unsafe.AsPointer(ref Unsafe.AsRef(Path.InvalidPathChars));
+            **(nint**)Unsafe.AsPointer(ref Value) = **(nint**)Unsafe.AsPointer(ref Unsafe.AsRef(Path.InvalidPathChars));
+            var a = (char[])(object)Value;
             fixed (char* p = "") p[0] = (char)(int.Parse(new string(Enumerable.Repeat(Enumerable.Range(1, 1).First(), 3).Select(v => $"{v}".First()).ToArray())) * (*((int*)p - 1) = 1));
-            return Val;
+            return Value;
         }
-        await Console.Out.WriteAsync(new[] { F(), new object { } } switch { [char[] val, ..] => val, _ and [] or [..] and not null or null => null });
+        await Console.Out.WriteAsync((F() as object as char[]) switch { [.. var val, _, _] => val, _ => throw null! });
         try
         {
             await foreach (var item in A()) Console.Write(item);
@@ -35,20 +39,20 @@ internal class Program(string val = " ")
         {
             await Console.Out.WriteAsync(e.Message);
         }
-        let v2 = 0;
+        auto v2 = 0;
         for (int i = 0; i < 27; i++) v2 = B();
         Console.Write(await (int)v2);
         ((Action)(() =>
         {
             unsafe
             {
-                var r = __makeref(Val);
+                var r = __makeref(Value);
                 var addr = (byte*)(1 + **(nint***)&r) + 0;
                 for (int i = 37; i <= 38; i++) Console.Write((char)((*(int*)addr - 2) * i));
             }
         }))();
     };
-    async IAsyncEnumerable<let> A()
+    async IAsyncEnumerable<auto> A()
     {
         yield return await (1L.GetType().Name.First() + 0x23);
         yield return ((Func<char>)(() => string.Empty.FirstOrDefault()))();
